f3ff13c1410d3c7e2341121404a7763672363055
Reece Jason <ambiently@outlook.com>
2025-09-18 19:31:56 +0100

    bits

--- PanoptoScheduleUploader.UI/Properties/AssemblyInfo.cs
+++ PanoptoScheduleUploader.UI/Properties/AssemblyInfo.cs
@@ -7,7 +7,7 @@ using System.Windows;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Panopto Schedule Uploader")]
+[assembly: AssemblyTitle("Panopto Schedule Uploader - Enhanced")]
 [assembly: AssemblyDescription("Uploader for Panopto recording schedules")]
 [assembly: AssemblyConfiguration("Release")]
 [assembly: AssemblyCompany("Aiimi")]
