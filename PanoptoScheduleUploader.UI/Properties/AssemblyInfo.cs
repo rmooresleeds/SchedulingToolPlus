af5e0f31153252fd0f8af7ebe48f9b5c0b3dc9cb
Reece Jason <ambiently@outlook.com>
2025-09-19 08:03:12 +0100

    assinfo

--- PanoptoScheduleUploader.UI/Properties/AssemblyInfo.cs
+++ PanoptoScheduleUploader.UI/Properties/AssemblyInfo.cs
@@ -7,10 +7,10 @@ using System.Windows;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Panopto Schedule Uploader - Enhanced")]
+[assembly: AssemblyTitle("Panopto Schedule Uploader Enhanced")]
 [assembly: AssemblyDescription("Uploader for Panopto recording schedules")]
 [assembly: AssemblyConfiguration("Release")]
-[assembly: AssemblyCompany("Aiimi")]
+[assembly: AssemblyCompany("Ambiently")]
 [assembly: AssemblyProduct("Panopto Schedule Uploader")]
 [assembly: AssemblyCopyright("Copyright © Ambiently 2013-2025")]
 [assembly: AssemblyTrademark("Ambiently")]
@@ -52,5 +52,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.0.0.0")]
-[assembly: AssemblyFileVersion("2.0.0.0")]
+[assembly: AssemblyVersion("2.0.0.1")]
+[assembly: AssemblyFileVersion("2.0.1.0")]
